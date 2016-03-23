31f38fca06c2e9a7a5739b831df16d0bcc2fdffa
jfmolano1587 <jose.francisco.molano@gmail.com>
2016-03-22 22:09:16 -0500

    V2
    HTTP

--- app/src/main/java/com/uniandes/jfm/pg20161client/MainActivity.java
+++ app/src/main/java/com/uniandes/jfm/pg20161client/MainActivity.java
@@ -1,9 +1,18 @@
 package com.uniandes.jfm.pg20161client;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +21,61 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new TareaRed().execute();
     }
 
+    private class TareaRed extends AsyncTask<URL, Integer, Long> {
+        protected Long doInBackground(URL... urls) {
+            try {
+                String url = "http://157.253.205.33/api/marcas";
+                URL object = new URL(url);
+
+                HttpURLConnection con = (HttpURLConnection) object.openConnection();
+                con.setDoOutput(true);
+                con.setDoInput(true);
+                con.setRequestProperty("Content-Type", "application/json");
+                con.setRequestProperty("Accept", "application/json");
+                con.setRequestMethod("POST");
+
+                JSONObject objetoJSON = new JSONObject();
+
+                objetoJSON.put("codigo", "Cod1");
+                objetoJSON.put("tiempo", "T1");
+
+                OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+                wr.write(objetoJSON.toString());
+                wr.flush();
+
+//display what returns the POST request
+
+                StringBuilder sb = new StringBuilder();
+                int HttpResult = con.getResponseCode();
+                if (HttpResult == HttpURLConnection.HTTP_OK) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
+                    String line = null;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line + "\n");
+                    }
+
+                    br.close();
+
+                    System.out.println("" + sb.toString());
+
+                } else {
+                    System.out.println(con.getResponseMessage());
+                }
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.fillInStackTrace());
+            }
+            return 0L;
+        }
+
+        protected void onPostExecute(Long result) {
+            System.out.println("Downloaded " + result + " bytes");
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
